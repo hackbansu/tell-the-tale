8f795c719afcb1aa0ec0d54bfdf602a4636de1fb
pawan bansal <bansal.pawan96@gmail.com>
2017-03-07 18:01:38 +0530

    Basic Server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pawan on 7/3/17.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+console.log(path.isAbsolute('/'));
+
+
+app.use('/', express.static(path.join(__dirname, "public_html")));
+
+app.listen(3333, function () {
+    console.log("server started at 3333");
+})
