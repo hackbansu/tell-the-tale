5f3e6b68d3686efc77e56cc2ac4d9181b2a78a8b
pawan bansal <bansal.pawan96@gmail.com>
2017-03-24 02:15:42 +0530

    updated main page

--- views/mainPage/js/script.js
+++ views/mainPage/js/script.js
@@ -2,17 +2,30 @@
  * Created by pawan on 21/3/17.
  */
 
-if (window.outerWidth > 992) {
-    $('.buttons').css('right', ($(".stories-container-outer").width() / 10) + (0.1 * ($(".stories-container-outer").width() / 10)));
+const shuffleButton = $('.shuffleButton');
+const newButton = $('.newButton');
+const shuffleButtonTextNode = shuffleButton.children("span:last");
+const newButtonTextNode = newButton.children("span:last");
+
+console.log(window.innerWidth);
+
+if (window.innerWidth <= 992) {
+    // $('.buttons').css('right', ($(".stories-container-outer").width() / 10) + (0.1 * ($(".stories-container-outer").width() / 10)));
+    shuffleButtonTextNode.text('');
+    newButtonTextNode.text('')
 }
+
 $(window).resize(function () {
     console.log("resizing")
-
-    if (window.outerWidth > 992) {
-        let columnWidth = $(".stories-container-outer").width() / 10;
-        $('.buttons').css('right', columnWidth + (0.1 * columnWidth));
+    if (window.innerWidth >= 992) {
+        // let columnWidth = $(".stories-container-outer").width() / 10;
+        // $('.buttons').css('right', columnWidth + (0.1 * columnWidth));
+        shuffleButtonTextNode.text(' Shuffle');
+        newButtonTextNode.text(' New')
     }
     else{
-        $('.buttons').css('right', 25);
+        // $('.buttons').css('right', 25);
+        shuffleButtonTextNode.text('');
+        newButtonTextNode.text('')
     }
 })
