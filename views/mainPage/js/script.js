cf1b49fc4902aced97e2f412f82e76ed42ca9391
pawan bansal <bansal.pawan96@gmail.com>
2017-03-28 02:27:11 +0530

    successfull ui changes

--- views/mainPage/js/script.js
+++ views/mainPage/js/script.js
@@ -29,3 +29,67 @@ $(window).resize(function () {
         newButtonTextNode.text('')
     }
 })
+
+
+//header hiding on scroll
+let didScroll = false;
+let lastScrollTop = $(window).scrollTop();
+let delta = 5;
+let header = $('.header-outer')
+let headerHeight = header.outerHeight();
+
+$(window).scroll(function () {
+    didScroll = true;
+})
+
+setInterval(function () {
+    if(didScroll){
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let st = $(this).scrollTop();
+
+    if(Math.abs(lastScrollTop - st) <= delta){
+        return;
+    }
+
+    if(st > lastScrollTop && st > headerHeight){
+        //scroll down
+        header.removeClass('nav-down').addClass('nav-up');
+    }
+    else{
+        //scroll up
+        if(st + $(window).height() < $(document).height()){
+            header.removeClass('nav-up').addClass('nav-down')
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+//expanding the categories div
+let categoriesContainer = $('.categories-container');
+let caretContainer = $('.caret-container');
+let catogoriesContainerCaret = $('.categories-container-outer .caret')
+let catogoriesContainerExpanded = false;
+
+caretContainer.click(function (ev) {
+    ev.stopPropagation();
+    if(!catogoriesContainerExpanded){
+        categoriesContainer.css('height', "15rem")
+        categoriesContainer.css('overflow', "auto")
+        catogoriesContainerCaret.css('border-top', '0px solid #000000');
+        catogoriesContainerCaret.css('border-bottom', '10px solid #000000');
+    }
+    else{
+        categoriesContainer.css('height', "calc(7rem - 10px)");
+        categoriesContainer.css('overflow', "hidden");
+        catogoriesContainerCaret.css('border-bottom', '0px solid #000000');
+        catogoriesContainerCaret.css('border-top', '10px solid #000000');
+    }
+
+    catogoriesContainerExpanded = !catogoriesContainerExpanded;
+})
